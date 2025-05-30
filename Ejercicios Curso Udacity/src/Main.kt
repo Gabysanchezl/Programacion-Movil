066bd11a26c0a31008e57f12e13784cde2eb0f98
set <2024-0142@unicda.edu.do>
2025-05-30 00:58:47 -0400

    Ejercicios Udacity Upload

---
+++ Ejercicios Curso Udacity/src/Main.kt
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*
+fun main() {
+    fun main() {
+        // Suma
+        val result1 = 1 + 1
+        println("1 + 1 = $result1") // Imprime: 1 + 1 = 2
+
+        // Hay 50 peces y llegan 3 más
+        var fish = 50 + 3
+        println("Total peces: $fish") // Imprime: Total peces: 53
+
+        // 3 peces bebé son comida de tiburones
+        fish -= 3
+        println("Peces después de la morision :v : $fish") // Imprime: Peces después del ataque: 50
+
+        // Dividimos los peces en poncheras de a 10 cada una
+        val batches = fish / 10
+        println("Grupos de 10 peces: $batches") // Imprime: 5
+    }
+
+}
