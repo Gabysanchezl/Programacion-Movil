a8b225541633b46b929cc31dd8532b1a9f49344c
set <2024-0142@unicda.edu.do>
2025-06-05 19:23:52 -0400

    Lab exerc.

--- Ejercicios 3/Laboratorio Teams/src/Main.kt
+++ Ejercicios 3/Laboratorio Teams/src/Main.kt
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     //Exercise 1: Optional User Details
+    println("Ejercicio #1--------------------")
+    println("  ")
     val userName: String? = null
     val userEmail: String? = "pedrito@oi.com"
     val userBio: String? = null
@@ -27,6 +29,64 @@ fun main() {
     println(biografiaPersonal)
 
     //Exercise 2: Simple To-Do List ✅
+
+        val todoList: MutableList<String?> = mutableListOf()
+
+        // Step 2: Add some tasks, including a null
+    println("Ejercicio #2--------------------")
+    println("  ")
+    todoList.add("Buy groceries")
+        todoList.add(null)
+        todoList.add("Walk the dog")
+        todoList.add("Finish Kotlin lab")
+
+        println("To-Do List:")
+        for (item in todoList) {
+            println(item ?: "[No task description]")
+        }
+
+        val firstTask = todoList.firstOrNull()
+            if (firstTask == null) {
+                println("First task: [No task description]")
+            } else {
+                println("First task: ${firstTask ?: "[No task description]"}") // MANEJA NULLABLE
+            }
+
+
+    // Ejercicio 3  - ITEM PRICES
+    println("Ejercicio #3--------------------")
+    println("  ")
+
+    val itemNames = arrayOf("Laptop", "Phone", "Headphones", "Mouse", "Charger")
+     val itemPrices: Array<Double?> = arrayOf(999.99, 699.50, null, 25.99, 19.99)
+
+        val index = 2
+        if (index in itemNames.indices && index in itemPrices.indices) {
+            val name = itemNames[index]
+            val price = itemPrices[index]
+
+            if (price != null) {
+                println("Item: $name, Price: $$price")
+            } else {
+                println("Item: $name, Price: Not available")
+            }
+        } else {
+            println("Invalid item index.")
+        }
+
+        /// Ejercicio 4 : Default Configuration Values ⚙️
+
+    println("Ejercicio #4--------------------")
+    println("  ")
+        var settingColorTheme: String? = null
+        var settingFontSize: Int? = 16
+
+
+        val actualColorTheme = settingColorTheme ?: "Light"
+        val actualFontSize = settingFontSize ?: 12
+
+        println("Applying theme: $actualColorTheme, Font size: ${actualFontSize}pt.")
+
 
 
 }
